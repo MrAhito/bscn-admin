41a5b74c05e2ce0d5fa835a82cb0df7dc2519854
MrAhito <72699331+MrAhito@users.noreply.github.com>
2022-06-28 07:48:44 +0800

    inputEdits

--- styles/addSubs.style.php
+++ styles/addSubs.style.php
@@ -1,7 +1,5 @@
 <style>
-
-
-.add_sub_block{
+.add_sub_block {
     position: absolute;
     background-color: rgba(194, 194, 194, .3);
     top: 50px;
@@ -14,7 +12,7 @@
 }
 
 .add_sub {
-    width: 60%;
+    width: 75%;
     overflow: hidden;
     border-radius: 5px;
     background-color: var(--primary);
@@ -34,11 +32,14 @@
 .add_head>h2 {
     color: var(--primary);
     font-size: 1.1em;
+
 }
-.add_head>h2 > i {
+
+.add_head>h2>i {
     font-size: 1.2em;
     margin-right: 5px;
 }
+
 .add_head>i {
     color: var(--primary);
     font-size: 1.65em;
@@ -85,72 +86,122 @@
 .gr3-2 {
     grid-column: 7/13;
 }
-.gr4-1{
-    grid-column: 1/3;}
-.gr4-2{
-    grid-column: 3/5;}
-.gr4-3{
-    grid-column: 5/7;}
-.gr4-4{
-    grid-column: 7/9;}
-.gr4-5{
-    grid-column: 9/11;}
-.gr4-6{
-    grid-column: 11/13;}
-.gr5-1{
-    grid-column: 1/4;}
-.gr5-2{
-    grid-column: 4/7;}
-.gr5-3{
-    grid-column: 7/10;}
-.gr5-4{
-    grid-column: 10/13;}
+
+.gr4-1 {
+    grid-column: 1/3;
+}
+
+.gr4-2 {
+    grid-column: 3/5;
+}
+
+.gr4-3 {
+    grid-column: 5/7;
+}
+
+.gr4-4 {
+    grid-column: 7/9;
+}
+
+.gr4-5 {
+    grid-column: 9/11;
+}
+
+.gr4-6 {
+    grid-column: 11/13;
+}
+
+.gr5-1 {
+    grid-column: 1/4;
+}
+
+.gr5-2 {
+    grid-column: 4/7;
+}
+
+.gr5-3 {
+    grid-column: 7/10;
+}
+
+.gr5-4 {
+    grid-column: 10/13;
+}
 
 
 .header1 {
     display: flex;
-    align-items: flex-end;
     justify-content: space-between;
     font-weight: 600;
+    border-radius: 4px;
+    overflow: hidden;
     font-size: .8em;
     border-bottom: 1px solid var(--secondary);
 }
-.bt_5{
+
+.header1>h2 {
+    margin: 0;
+    width: 150px;
+    padding: 8px;
+    display: flex;
+    align-items: center;
+    font-size: .85em;
+    color: var(--primary);
+    background: linear-gradient(to left top, var(--secondary), var(--tertiary2));
+}
+
+.email {
+    text-transform: lowercase !important;
+}
+
+.email::placeholder {
+    text-transform: uppercase !important;
+}
+
+.bt_5 {
     margin-top: 20px;
 }
+
 .form_add>form-control {
     display: flex;
     justify-content: space-between;
     align-items: center;
 }
 
-.form_add>form-control> label {
+.form_add>form-control>label>pre {
     font-size: 0.8em;
-    margin-right: 5px;
+    margin: 0;
+    padding: 0 !important;
     font-weight: 600;
 }
 
 .form_add>form-control>input,
 .form_add>form-control>select {
-    width: 70%;
+    width: 75%;
     background-color: var(--fontDark);
     filter: brightness(1.25);
     font-size: 0.75em;
     border-radius: 5px;
     border: none;
     text-transform: uppercase;
-    padding: 8px ;
+    padding: 8px;
     border-bottom: 1px solid var(--fontDark2);
     outline: none;
 }
-.form_add>form-control>input:not(:placeholder-shown){
-  border-color: var(--secondary);
-  box-shadow: 1px 1px 5px var(--secondary);
+
+.form_add>form-control>select {
+    width: 80%;
 }
+
+.form_add>form-control>input:not(:placeholder-shown) {
+    border-color: var(--secondary);
+    box-shadow: 1px 1px 5px var(--secondary);
+}
+
 .form_add>form-control>select:valid {
-  box-shadow: 1px 1px 5px var(--secondary);
-  border-color: var(--secondary);
+    box-shadow: 1px 1px 5px var(--secondary);
+    border-color: var(--secondary);
 }
+
 .form_add>form-control>select>option {
     font-size: 1.1em;
     color: var(--primary);
@@ -161,8 +212,8 @@
 .form_add>form-control>input:focus,
 .form_add>form-control>select:hover,
 .form_add>form-control>select:focus {
-  box-shadow: 1px 1px 5px var(--secondary);
-  border-bottom: 1px solid var(--secondary);
+    box-shadow: 1px 1px 5px var(--secondary);
+    border-bottom: 1px solid var(--secondary);
 }
 
 .opt_brgy {
@@ -174,21 +225,24 @@
 .ori {
     display: flex;
 }
-.form-c-btn{
-display: flex;
-align-items: center;
-justify-content: end;
-}
-.addFormBtn{
-    display:flex;
+
+.form-c-btn {
+    display: flex;
     align-items: center;
-    font-size:.9em;
-    border-radius: 4px;
-    font-weight: 600;
-    padding: 7px;
+    justify-content: end;
 }
-.addFormBtn > i{
-    font-size:1.2em;
-    margin-right:5px;
+
+.addFormBtn {
+    display: flex;
+    align-items: center;
+    font-size: .85em;
+    border-radius: 4px;
+    padding: 0 10px;
+    font-weight: 600;
+}
+
+.addFormBtn>i {
+    font-size: 1.2em;
+    margin-right: 5px;
 }
 </style>
---
+++ styles/subsInfo.style.php
@@ -0,0 +1,149 @@
+<style>
+.p_info {
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+    height: fit-content;
+    flex-wrap: wrap;
+    width: 100%;
+}
+
+.inf_ {
+    grid-template-columns: repeat(22, 1fr);
+    row-gap: 12px;
+
+}
+
+.inf_head>a {
+    cursor: pointer;
+    color: var(--primary);
+    text-decoration: none;
+    padding: 7px;
+    display: flex;
+    align-items: center;
+    font-size: 1.175em;
+}
+
+.inf_head>a>i {
+    margin-right: 5px;
+}
+
+.inf_head>a:hover {
+    color: var(--fontDark);
+    transition: .3s all ease-in-out;
+}
+
+.form_add>form-control>select:valid,
+.form_add>form-control>input,
+.form_add>form-control>select,
+.form_add>form-control>input:not(:placeholder-shown) {
+    border-bottom: 1px solid var(--fontDark2);
+    box-shadow: none;
+    pointer-events: none;
+}
+
+.editInput {
+    pointer-events: visible !important;
+    box-shadow: 1px 1px 5px var(--secondary) !important;
+}
+
+.gr1 {
+    grid-column: 1/23;
+}
+
+.gr6-1 {
+    grid-column: 2/6;
+}
+
+.gr6-2 {
+    grid-column: 6/10;
+}
+
+.gr6-3 {
+    grid-column: 10/14;
+}
+
+.gr6-4 {
+    grid-column: 14/18;
+}
+
+.gr6-5 {
+    grid-column: 18/22;
+}
+
+.gr7-1 {
+    grid-column: 2/8;
+}
+
+.gr7-2 {
+    grid-column: 8/14;
+}
+
+.gr7-3 {
+    grid-column: 14/20;
+}
+
+.gr8-1 {
+    grid-column: 2/7;
+}
+
+.gr8-2 {
+    grid-column: 7/12;
+}
+
+.gr8-3 {
+    grid-column: 12/17;
+}
+
+.gr8-4 {
+    grid-column: 17/22;
+}
+
+.bt_5 {
+    margin-top: 10px;
+}
+
+.divEdit {
+    display: none;
+    margin: 0;
+    overflow: hidden;
+    justify-content: space-between;
+    border-radius: 4px;
+    width: 190px;
+}
+
+.sinBtn {
+    justify-content: flex-end;
+}
+
+.douBtn {
+    justify-content: space-between;
+}
+
+.showInfBtn {
+    display: flex;
+}
+
+.subsInfBtn {
+    font-size: .9em;
+    width: 85px;
+}
+
+.subsInfBtn>i {
+    font-size: .9em;
+    margin-right: 5px;
+}
+
+.canc {
+    background-color: var(--primary);
+    color: var(--secondary);
+    transition: .3s all ease-in-out;
+}
+
+.canc:hover {
+    border-color: var(--red);
+    background-color: var(--red);
+    color: var(--primary);
+    transition: .3s all ease-in-out;
+}
+</style>
